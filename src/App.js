8a7bbc2efc1d0dd36240183dfad142d57433aeb9
Chunrong <56704548+wangchunrong1983@users.noreply.github.com>
2020-07-08 15:07:43 -0700

    setup shop page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
-const HatsPage = () => (
-  <div>
-   <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />   
+        <Route path='/shop' component={ShopPage} />   
       </Switch>
     </div>
   );
